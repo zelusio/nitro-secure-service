c326897141aedb5b604e1dad0f6e641661782cb0
Kyle Mistele <kyle@mistele.com>
2023-06-13 16:46:11 -0500

    add skeleton

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, {Express, Request, Response} from 'express';
+
+const app: Express = express();
+
+app.get('/', (req: Request, res: Response)=>{
+    res.send('Hello, this is Express + TypeScript');
+});
+
+export default app
+
