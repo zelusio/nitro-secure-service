b2117c53dffd1bcb92bc1d68c325894ac3981cb5
vm-vchyypesh <vchyypesh@visiblemagic.com>
2024-09-23 12:18:54 +0300

    refactor: add default bucket names related to env

--- src/services/jwk/s3.config.ts
+++ src/services/jwk/s3.config.ts
@@ -2,6 +2,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const { ENVIRONMENT } = process.env;
+
 export const REGION = 'us-east-1';
-export const BUCKET_NAME = process.env.BUCKET_NAME || 'nitro-secure-service-keys-dev';
-export const S3_KEY = process.env.S3_KEY || 'encrypted-keys.txt';
+export const BUCKET_NAME = process.env.BUCKET_NAME || getDefaultBucketName(ENVIRONMENT);
+export const S3_KEY = process.env.S3_KEY || `encrypted-keys-${ENVIRONMENT}.txt`;
+
+function getDefaultBucketName(ENVIRONMENT: string | undefined): string {
+  switch (ENVIRONMENT) {
+    case 'staging':
+      return 'nitro-secure-service-keys-staging';
+    case 'prod':
+      return 'nitro-secure-service-keys-prod';
+    default:
+      return 'nitro-secure-service-keys-dev';
+  }
+}
