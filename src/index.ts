277d168c27b5bd8f504ff9510541b348239a2a43
vm-vchyypesh <vchyypesh@visiblemagic.com>
2024-09-23 17:26:44 +0300

    style: update eslint config, add mocha and security plugins

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,6 @@ const port = process.env.PORT || 3000;
 const app = createExpressApp();
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`[Server]: Running at port ${port}`);
 });
