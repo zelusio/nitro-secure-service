70e75900230defe5ffd4a63425c405615789f7b0
vm-vchyypesh <vchyypesh@visiblemagic.com>
2024-10-09 10:55:19 +0300

    test: add request to dev enclave with attestation

--- tests/well-known.router.spec.ts
+++ tests/well-known.router.spec.ts
@@ -3,6 +3,9 @@ import chaiHttp from 'chai-http';
 import { Express } from 'express';
 import axios from 'axios';
 import { createExpressApp } from '../src/app';
+import { secrets } from '../src/services/secrets.service';
+import EvervaultClient from '@evervault/sdk';
+import AttestationBindings from '@evervault/attestation-bindings';
 
 chai.use(chaiHttp);
 
@@ -25,16 +28,57 @@ describe('Well-Known API Tests', function () {
     expect(res.body[0]).to.have.property('n');
   });
 
-  describe('e2e requests to dev', function () {
-    it('GET DEV env /.well-known/jwks.json should return 200', async function () {
-      const DEV_URL = 'https://nitro-secure-service-dev.app-b81dd8dc1c0d.cage.evervault.com';
+  describe('Requests to DEV enclave', function () {
+    const DEV_URL = 'https://nitro-secure-service-dev.app-b81dd8dc1c0d.cage.evervault.com';
+
+    it('GET /.well-known/jwks.json for DEV url should return 200', async function () {
+      const res = await axios.get(DEV_URL + '/.well-known/jwks.json', {
+        headers: {
+          'Content-Typ': 'application/json',
+          'api-key': secrets.EVERVAULT_API_KEY
+        },
+        validateStatus: () => true
+      });
+
+      expect(res.status).to.equal(200);
+      expect(res.data).to.have.property('length');
+      expect(res.data[0]).to.have.property('kty');
+      expect(res.data[0]).to.have.property('kid');
+      expect(res.data[0]).to.have.property('use');
+      expect(res.data[0]).to.have.property('alg');
+      expect(res.data[0]).to.have.property('e');
+      expect(res.data[0]).to.have.property('n');
+    });
+
+    it('GET /.well-known/jwks.json for DEV url with attestation should return 200', async function () {
+      this.timeout(10000);
+
+      const cageName = 'nitro-secure-service-dev';
+
+      const pcrResponse = await axios.get(`https://api.evervault.com/enclaves/${cageName}/attestation`, {
+        headers: {
+          'x-evervault-app-id': secrets.EVERVAULT_APP_ID
+        }
+      });
+
+      const pcrList = pcrResponse.data.data;
+
+      const evervaultClient = new EvervaultClient(secrets.EVERVAULT_APP_ID, secrets.EVERVAULT_API_KEY);
+
+      const enclaveHttpsAgent = await evervaultClient.createEnclaveHttpsAgent(
+        {
+          [cageName]: pcrList
+        },
+        AttestationBindings,
+        {}
+      );
 
       const res = await axios.get(DEV_URL + '/.well-known/jwks.json', {
         headers: {
           'Content-Typ': 'application/json',
-          'api-key': process.env.EV_API_KEY
+          'api-key': secrets.EVERVAULT_API_KEY
         },
-        // httpsAgent: enclaveHttpsAgent,
+        httpsAgent: enclaveHttpsAgent,
         validateStatus: () => true
       });
 
