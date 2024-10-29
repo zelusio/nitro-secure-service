be7e810105dc7a641536c4e4502bad850b558449
vm-vchyypesh <vchyypesh@visiblemagic.com>
2024-10-29 18:17:44 +0200

    refactor: replace jwk service from auth lib, update jwk provider to use cache

--- src/services/exportWallet.service.ts
+++ src/services/exportWallet.service.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import crypto from 'node:crypto';
 import { JWTIssuer, Scope } from '@zelusio/auth-lib';
-import JWKService from './jwk/jwk.service';
 import { secrets } from './secrets.service';
+import { getJWKProviderInstance } from './jwk/jwkProvider';
 
 export interface IAuthWalletEncryptedBody {
   jwt_user: string;
@@ -13,9 +13,9 @@ export interface IAuthWalletEncryptedResponse {
 }
 
 export async function getEncryptedWallet(authToken: string, subject: string): Promise<string> {
-  const keyServiceInstance = await JWKService.getProviderInstance();
+  const jwkProvider = getJWKProviderInstance();
 
-  const jwtIssuer = new JWTIssuer(secrets.NSS_ISSUER, await keyServiceInstance.getDefaultSigningKey());
+  const jwtIssuer = new JWTIssuer(secrets.NSS_ISSUER, await jwkProvider.getDefaultPrivateKey());
   const nssToken = await jwtIssuer.issueAccessToken(subject, [], [Scope.InvisibleWalletExport]);
   const walletEncrypted = await getEncryptedWalletForUser(authToken, nssToken);
 
--- src/services/jwk/jwkProvider.ts
+++ src/services/jwk/jwkProvider.ts
@@ -1,64 +1,95 @@
-import jose from 'node-jose';
+import { JWKService } from '@zelusio/auth-lib';
+import logService from '../logging.service';
+import JwkS3Store from './jwkS3Store';
+import S3JobLockService from './s3JobLock.service';
 import { IJwkExternalStore } from './IJwkExternalStore';
 
-export default class JWKProvider {
-  private readonly keyStore: jose.JWK.KeyStore;
-  private readonly jwkExternalStore: IJwkExternalStore;
-  private readonly keyIds: string[] = [];
+const CACHE_EXPIRATION = 5 * 60 * 1000; // 5 minutes in milliseconds
+const KEY_ROTATION_LOCK = 'key-rotation';
+const LOCK_EXPIRATION_TIME = 30 * 1000; // 30 seconds
 
-  constructor(keyStore: jose.JWK.KeyStore, jwkExternalStore: IJwkExternalStore) {
-    this.keyStore = keyStore;
-    this.keyStore.all().forEach((key: jose.JWK.Key) => this.keyIds.push(key.kid!));
+let _jwkProvider: JWKProvider | undefined;
+
+export function getJWKProviderInstance(jwkExternalStore: IJwkExternalStore = new JwkS3Store()): JWKProvider {
+  if (!_jwkProvider) {
+    _jwkProvider = new JWKProvider(jwkExternalStore);
+  }
+  return _jwkProvider;
+}
+
+export default class JWKProvider {
+  private lastCreated: number = 0;
+  private jwkService: JWKService | undefined;
+  private jwkExternalStore: IJwkExternalStore;
+  private jobLockService: S3JobLockService = new S3JobLockService();
+
+  constructor(jwkExternalStore: IJwkExternalStore) {
     this.jwkExternalStore = jwkExternalStore;
   }
 
-  /**
-   * Return the **public** portion of all keys in the keystore
-   * @returns {jose.JWK.Key[]}
-   */
-  public getPublicKeys(): jose.JWK.Key[] {
-    return this.keyStore.all().map((key: jose.JWK.Key) => key.toJSON(false) as jose.JWK.Key);
+  public async getPublicKeys() {
+    const jwkService = await this.getJWKServiceInstance();
+    return jwkService.getPublicKeys();
   }
 
-  /**
-   * Get a key from the keystore to use for signing. This is the first key in the keystore
-   * @returns {jose.JWK.Key}
-   */
-  public getDefaultSigningKey(): jose.JWK.Key {
-    return this.keyStore.get({ use: 'sig', kty: 'RSA', kid: this.keyIds[0] }).toJSON(true) as jose.JWK.Key;
+  public async getDefaultPrivateKey() {
+    const jwkService = await this.getJWKServiceInstance();
+    return jwkService.getDefaultPrivateKey();
   }
 
-  /**
-   * Find the specified key from the keystore
-   * @param {string} kid - the key ID
-   * @param {string} kty - the key type (should be `RSA`)
-   * @param {string} alg - should be (`RS256`)
-   * @returns {jose.JWK.Key} - the JWK including private portions
-   */
-  public findKey(kid: string, kty: string, alg: string): jose.JWK.Key | undefined {
-    return this.keyStore.get({ kid, kty, alg })?.toJSON(true) as jose.JWK.Key;
-  }
+  private async getJWKServiceInstance(): Promise<JWKService> {
+    if (Date.now() - this.lastCreated > CACHE_EXPIRATION || !this.jwkService) {
+      logService.debug('Looking for JWK keys in external storage...');
+      const storedKeys = (await this.jwkExternalStore.getKeys()) as any[];
 
-  public async rotateKeys(): Promise<void> {
-    const newKey = await this.keyStore.generate('RSA', 2048, {
-      alg: 'RS256',
-      use: 'sig'
-    });
-
-    // put new key in front
-    this.keyIds.unshift(newKey.kid);
-
-    // keep only the last 2 keys
-    if (this.keyIds.length > 2) {
-      // get the last key from the id list
-      const keyId = this.keyIds.pop() as string;
-      const keyToRemove = this.keyStore.get(keyId);
-      if (keyToRemove) {
-        this.keyStore.remove(keyToRemove);
+      if (storedKeys?.length > 0) {
+        logService.debug(`Found ${storedKeys.length} JWK keys in external storage.`);
+        this.jwkService = new JWKService(storedKeys);
+        logService.debug('Created JWKService instance with existing keys from external storage');
+      } else {
+        logService.debug('Not found any JWK keys in external storage.');
+        this.jwkService = await this.createJWKAndStoreKeys();
       }
+
+      this.lastCreated = Date.now();
     }
 
-    const newKeys = this.keyStore.all().map((key: jose.JWK.Key) => key.toJSON(true) as jose.JWK.Key);
-    await this.jwkExternalStore.saveKeys(newKeys);
+    return this.jwkService;
+  }
+
+  private async createJWKAndStoreKeys(): Promise<JWKService> {
+    logService.debug('Creating JWKService instance...');
+    const jwkService = new JWKService();
+    logService.debug('Generating new keys...');
+    const keys = await jwkService.getPrivateKeys();
+    logService.debug('Retrieved new keys. Saving to external storage...');
+    await this.jwkExternalStore.saveKeys(keys);
+    logService.debug('Finished saving new keys to external storage.');
+    return jwkService;
+  }
+
+  public async rotateKeys() {
+    const locked = await this.jobLockService.tryLockJob(KEY_ROTATION_LOCK, LOCK_EXPIRATION_TIME);
+    if (!locked) {
+      logService.log(`JWKService key rotation is skipped for job: ${KEY_ROTATION_LOCK}`);
+      return;
+    }
+
+    try {
+      logService.log('JWKService is rotating keys...');
+      const jwkService = await this.getJWKServiceInstance();
+      await jwkService.rotateKeys();
+      logService.log('JWKService rotated keys!');
+
+      const keys = await jwkService.getPrivateKeys();
+      logService.debug('Retrieved rotated keys. Saving to external storage...');
+      await this.jwkExternalStore.saveKeys(keys);
+      logService.debug('Finished saving rotated keys to external storage.');
+    } catch (err: unknown) {
+      logService.log('An error occurred while the JWKService was rotating keys!');
+      logService.error(err);
+    } finally {
+      await this.jobLockService?.unlockJob(KEY_ROTATION_LOCK);
+    }
   }
 }
